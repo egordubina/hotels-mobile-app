9dbafbbac4f589af2b56867e3d54db74ecb3ef7b
Egor Dubina <egordubina@icloud.com>
2023-11-17 09:12:01 +0300

    рефакторнг, доработка экрана комнат

--- app/src/main/java/ru/egordubina/hotels/screens/ChoiceApartments.kt
+++ app/src/main/java/ru/egordubina/hotels/screens/ChoiceApartments.kt
@@ -15,6 +15,7 @@ import androidx.navigation.fragment.navArgs
 import com.google.android.material.transition.MaterialSharedAxis
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import ru.egordubina.domain.models.Apartment
 import ru.egordubina.hotels.R
 import ru.egordubina.hotels.adapters.ApartmentAdapterItemDecoration
 import ru.egordubina.hotels.adapters.ApartmentsAdapter
@@ -22,6 +23,7 @@ import ru.egordubina.hotels.adapters.ImageSliderAdapter
 import ru.egordubina.hotels.databinding.FragmentChoiceOfApartmentsBinding
 import ru.egordubina.hotels.uistates.ApartmentsScreenUiState
 import ru.egordubina.hotels.utils.addChipToPeculiarities
+import ru.egordubina.hotels.utils.toPx
 import ru.egordubina.hotels.viewmodels.ApartmentsScreenViewModel
 
 @AndroidEntryPoint
@@ -44,26 +46,29 @@ class ChoiceApartments : Fragment(R.layout.fragment__choice_of_apartments) {
                         loadingIndicator.isVisible = uiState == ApartmentsScreenUiState.Loading
                     }
                     when (uiState) {
-                        is ApartmentsScreenUiState.Content -> {
-                            binding.apply {
-                                rvApartments.adapter = ApartmentsAdapter(
-                                    apartmentsItems = uiState.apartments,
-                                    setImagesSliderAdapter = { urls -> ImageSliderAdapter(urls) },
-                                    setChips = {
-                                        it.map { chipText ->
-                                            addChipToPeculiarities(requireContext(), chipText)
-                                        }
-                                    }
-                                ) { findNavController().navigate(R.id.action_choiceApartments_to_successPay) }
-                                rvApartments.addItemDecoration(ApartmentAdapterItemDecoration(bottom = 8))
-                            }
-                        }
+                        is ApartmentsScreenUiState.Content -> showContent(apartments = uiState.apartments)
 
                         ApartmentsScreenUiState.Error -> {}
                         ApartmentsScreenUiState.Loading -> {}
                     }
                 }
             }
+        }
+    }
+
+    private fun showContent(apartments: List<Apartment>) {
+        binding.apply {
+            rvApartments.adapter = ApartmentsAdapter(
+                apartmentsItems = apartments,
+                setImagesSliderAdapter = { urls -> ImageSliderAdapter(urls) },
+                setChips = {
+                    it.map { chipText -> addChipToPeculiarities(requireContext(), chipText) }
+                },
+                onButtonClick = { findNavController().navigate(R.id.action_choiceApartments_to_successPay) }
+            )
+            rvApartments.addItemDecoration(
+                ApartmentAdapterItemDecoration(bottom = requireContext().toPx(8).toInt())
+            )
         }
     }
 
